ceb68ccd691f4f71f4b27f99082bd4e2e0999a4f
Shaoyang Ye <shaoyangye233@gmail.com>
2019-09-11 00:53:38 +1000

    Add sign in UI design with storyboard.

---
+++ Lunchat/SigninViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SigninViewController.swift
+//  Lunchat
+//
+//  Created by JamesCullen on 2019/9/11.
+//  Copyright © 2019 MobileTeam. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

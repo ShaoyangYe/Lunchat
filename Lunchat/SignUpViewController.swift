53044136c469f1a545561981ff271ba16f8a4246
Shaoyang Ye <shaoyangye233@gmail.com>
2019-09-11 12:58:46 +1000

    Add Sign Up view dismiss function.

---
+++ Lunchat/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  Lunchat
+//
+//  Created by JamesCullen on 2019/9/11.
+//  Copyright © 2019 MobileTeam. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

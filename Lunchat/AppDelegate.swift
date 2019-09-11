03d88e682f78c7481e306322c087be1585d510ba
Shaoyang Ye <shaoyangye233@gmail.com>
2019-09-11 18:40:32 +1000

    Sign In and Sign Up button color change into Orange, tab bar item color set into black after pressing.

--- Lunchat/AppDelegate.swift
+++ Lunchat/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Change all Tab Bar Item color into black.
+        UITabBar.appearance().tintColor = .black
         return true
     }
 

2b8f89303cff6030dc2d6d4ec61ac63046fc009b
Shaoyang Ye <shaoyangye233@gmail.com>
2019-09-13 23:04:25 +1000

    Link UI components in Sign In and Sign Up with code, reshape profile image.

--- Lunchat/SignUpViewController.swift
+++ Lunchat/SignUpViewController.swift
@@ -10,24 +10,24 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Change to profileImage shape into circle.
+        profileImage.layer.cornerRadius = 40
+        profileImage.clipsToBounds = true
+       
     }
     
     @IBAction func dismiss_onClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }

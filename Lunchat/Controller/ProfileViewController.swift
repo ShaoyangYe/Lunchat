f7536f4d020aa5f861653e103e77f811e3fbcf21
Shaoyang Ye <shaoyangye233@gmail.com>
2019-10-21 13:35:32 +1100

    Link profile VC UI components with code.

--- Lunchat/Controller/ProfileViewController.swift
+++ Lunchat/Controller/ProfileViewController.swift
@@ -13,18 +13,23 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var lunchatView: UIView!
     @IBOutlet weak var lunchatMate: UIView!
     @IBOutlet weak var likes: UIView!
-    @IBOutlet weak var profileimage: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var editButton: UIButton!
     
-    @IBOutlet weak var characteristicLabel1: UILabel!
-    @IBOutlet weak var characteristicLabel2: UILabel!
-    @IBOutlet weak var characteristicLabel3: UILabel!
-    @IBOutlet weak var characteristicLabel4: UILabel!
+    @IBOutlet weak var lunchatCount: UILabel!
+    @IBOutlet weak var lunchMateCount: UILabel!
+    @IBOutlet weak var likesCount: UILabel!
     
-    @IBOutlet weak var skillLabel1: UILabel!
-    @IBOutlet weak var skillLabel2: UILabel!
-    @IBOutlet weak var skillLabel3: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var eduBackgroundLabel: UILabel!
+    
+    @IBOutlet weak var schoolLabel: UILabel!
+    @IBOutlet weak var majorLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var currentResidenceLabel: UILabel!
+    @IBOutlet weak var originalResidenceLabel: UILabel!
     
     @IBOutlet weak var logoutButton: UIBarButtonItem!
     
@@ -35,26 +40,10 @@ class ProfileViewController: UIViewController {
         likes.layer.cornerRadius = 5
         
         editButton.layer.cornerRadius = 5
-//        characteristicLabel1.layer.borderWidth = 2
-//        characteristicLabel1.layer.cornerRadius = 6
-//        characteristicLabel1.layer.borderColor = UIColor.darkGray.cgColor
-//
-//        characteristicLabel2.layer.borderWidth = 2
-//        characteristicLabel2.layer.cornerRadius = 6
-//        characteristicLabel2.layer.borderColor = UIColor.darkGray.cgColor
-//        
-//        characteristicLabel3.layer.borderWidth = 2
-//        characteristicLabel3.layer.cornerRadius = 6
-//        characteristicLabel3.layer.borderColor = UIColor.darkGray.cgColor
-//        
-//        characteristicLabel4.layer.borderWidth = 2
-//        characteristicLabel4.layer.cornerRadius = 6
-//        characteristicLabel4.layer.borderColor = UIColor.darkGray.cgColor
-//        
-        
-        profileimage.layer.cornerRadius = 60
-        profileimage.layer.borderWidth = 5
-        profileimage.layer.borderColor = UIColor.white.cgColor
+   
+        profileImage.layer.cornerRadius = 60
+        profileImage.layer.borderWidth = 5
+        profileImage.layer.borderColor = UIColor.white.cgColor
 
     self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
     self.navigationController?.navigationBar.isTranslucent = false

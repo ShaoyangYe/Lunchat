cfc16bcd5465273c0760e04b5486d2acc8da24f4
jiaqingc <jiaqingc@student.unimelb.edu.au>
2019-10-22 23:35:10 +1100

    update Manage page

--- Lunchat/Event.swift
+++ Lunchat/Event.swift
@@ -6,33 +6,19 @@
 //  Copyright © 2019 MobileTeam. All rights reserved.
 //
 
-import UIKit
-
 import Foundation
 
-class EventModel {
-    var eventID: String?
+struct Event {
+    
+    var eventID: Int?
     var host: String?
     var location: String?
-    var maxParticipant: Int?
-    var past: String?
+    var maxParticipants: Int?
+    var participants: [String]?
+    var past: Int?
     var theme: String?
     var time: String?
     var title: String?
+    
 }
 
-extension EventModel {
-    static func transformEvent(dict: [String: Any], key: String) -> EventModel {
-        let event = EventModel()
-        event.eventID = dict["eventID"] as? String
-        event.host = dict["host"] as? String
-        event.location = dict["location"] as? String
-        event.maxParticipant = dict["maxParticipant"] as? Int
-        event.past = dict["past"] as? String
-        event.theme = dict["theme"] as? String
-        event.time = dict["time"] as? String
-        event.title = dict["title"] as? String
-        
-        return event
-    }
-}

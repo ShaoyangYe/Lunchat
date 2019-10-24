d81de2e8487f5b602cdfb2594312ba61b0206474
Shaoyang Ye <shaoyangye233@gmail.com>
2019-10-24 17:01:08 +1100

    Edit Profile VC UI design completed, linked with code, can display data from Firebase.

--- Lunchat/Controller/SettingTableViewController.swift
+++ Lunchat/Controller/SettingTableViewController.swift
@@ -10,81 +10,81 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var changeProfileImageButton: UIButton!
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var eduBackgroundText: UITextField!
+    
+    @IBOutlet weak var schoolText: UITextField!
+    @IBOutlet weak var majorText: UITextField!
+    
+    @IBOutlet weak var companyText: UITextField!
+    @IBOutlet weak var positionText: UITextField!
+    
+    @IBOutlet weak var currentResidenceText: UITextField!
+    @IBOutlet weak var originalResidenceText: UITextField!
+   
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        profileImage.layer.cornerRadius = 60
+        profileImage.layer.borderWidth = 5
+        profileImage.layer.borderColor = UIColor.white.cgColor
+        
+        self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+        
+        fetchCurrentUser()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    func fetchCurrentUser() {
+        Api.User.observeCurrentUser { (user) in
+            self.nameText.text = user.username
+            self.emailText.text = user.email
+            if let photoUrlString = user.profileImageUrl {
+                let photoUrl = URL(string: photoUrlString)
+                let data = try? Data(contentsOf: photoUrl!)
+                self.profileImage.image = UIImage(data: data!)
+            }
+            self.eduBackgroundText.text = user.eduBackground!
+            
+            self.schoolText.text = user.school!
+            self.majorText.text = user.major!
+            
+            self.companyText.text = user.company!
+            self.positionText.text = user.position!
+            
+            self.currentResidenceText.text = user.currentResidence!
+            self.originalResidenceText.text = user.originalResidence!
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func saveBtn_TouchUpInside(_ sender: Any) {
+//        if let profileImg = self.profileImage.image, let imageData = profileImg.jpegData(compressionQuality: 0.1){
+//            ProgressHUD.show("Waiting...")
+//            AuthService.updateUserInfor(username: nameText.text!, email: emailText.text!, imageData: imageData, onSuccess: {
+//                ProgressHUD.showSuccess("Success")
+//                self.delegate?.updateUserInfor()
+//            }, onError: { (errorMessage) in
+//                ProgressHUD.showError(errorMessage)
+//            })
+//        }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func logoutBtn_TouchUpInside(_ sender: Any) {
+//        AuthService.logout(onSuccess: {
+//            let storyboard = UIStoryboard(name: "Start", bundle: nil)
+//            let signInVC = storyboard.instantiateViewController(withIdentifier: "SignInViewController")
+//            self.present(signInVC, animated: true, completion: nil)
+//        }) { (errorMessage) in
+//            ProgressHUD.showError(errorMessage)
+//        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

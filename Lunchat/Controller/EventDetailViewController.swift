e603d02b860a596dab890e64f4efe6a6c47f7395
jiaqingc <jiaqingc@student.unimelb.edu.au>
2019-10-25 00:08:35 +1100

    update manage!

--- Lunchat/Controller/EventDetailViewController.swift
+++ Lunchat/Controller/EventDetailViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import MapKit
+import Firebase
 
 protocol EventDetailViewControllerProtocol {
+    
     func dialogDismissed()
+    func displayMap()
+    
 }
 
 class EventDetailViewController: UIViewController {
@@ -33,13 +38,34 @@ class EventDetailViewController: UIViewController {
     
     var delegate:EventDetailViewControllerProtocol?
     
+    var dbRef:DatabaseReference?
+    
+    var currentEventID: Int?
+    var allParticipants = [Participants]()
+    var participants: [String:String]?
+    var participantsID: [String]!
+    var registeredParticipants = [Participants]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Customize the popup box
         backgroundView.layer.cornerRadius = 10
-        backgroundView.layer.borderWidth = 5
+        backgroundView.layer.borderWidth = 2
         backgroundView.layer.borderColor = UIColor(red:0.98, green:0.50, blue:0.45, alpha:1.0).cgColor
-       // backgroundView.layer.borderColor = UIColor.
+        cancelButton.viewWithTag(1)?.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 10)
+        
+        participantsTableView.delegate = self
+        participantsTableView.dataSource = self
+        getParticipants()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Read data from firebase
+        
+        participantsTableView.reloadData()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,16 +77,114 @@ class EventDetailViewController: UIViewController {
         locationLabel.text = locationText
         participantsLabel.text = participantsText
         cancelButton.setTitle(buttonText, for: .normal)
+        
+        delegate?.displayMap()
+        
+    }
+    
+    func getParticipants() {
+    
+        dbRef = Database.database().reference().child("users")
+        
+        dbRef!.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            let snapshots = snapshot.children.allObjects as! [DataSnapshot]
+            
+            for snap in snapshots {
+                
+                let participantsDict = snap.value as! [String:Any]
+                
+                let id = snap.key
+                let username = participantsDict["username"] as! String
+                let profileImageUrl = participantsDict["profileImageUrl"] as! String
+                print(profileImageUrl)
+                let p = Participants(id: id, username: username, profileImageUrl: profileImageUrl)
+                
+                self.allParticipants.append(p)
+                
+            }
+            
+        })
+        
+        participantsID = Array(participants!.keys)
+        print("hhhhh")
+    
     }
     
     @IBAction func dismissTapped(_ sender: Any) {
         
+        participantsID.removeAll()
+        print(currentEventID!)
         // Dismiss the popup
         self.dismiss(animated: true, completion: nil)
         
         // Notify delegat that popup was dismissed
         delegate?.dialogDismissed()
-        
     }
     
+}
+
+extension EventDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var i = 0
+        let count = self.participantsID!.count
+        
+        while i < count {
+            
+            for n in self.allParticipants {
+                
+                if self.participantsID[i] == n.id {
+                    
+                    print("add one participant")
+                    self.registeredParticipants.append(n)
+                    
+                }
+                
+            }
+            
+            i += 1
+            
+        }
+        
+        return self.registeredParticipants.count
+        
+    }
+    //participantsCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "participantsCell", for: indexPath)
+        
+        let image = cell.viewWithTag(10) as? UIImageView
+        let usernameLabel = cell.viewWithTag(11) as? UILabel
+        
+        if indexPath.row < registeredParticipants.count {
+            
+            let url = registeredParticipants[indexPath.row].profileImageUrl
+            image!.image = UIImage.init(named: url!)
+            usernameLabel!.text = registeredParticipants[indexPath.row].username
+            
+        }
+        return cell
+    }
+    
+    
+    
+}
+
+extension UIView {
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        
+        DispatchQueue.main.async {
+            let path = UIBezierPath(roundedRect: self.bounds,
+                                    byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: radius, height: radius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.frame = self.bounds
+            maskLayer.path = path.cgPath
+            self.layer.mask = maskLayer
+        }
+    }
 }
